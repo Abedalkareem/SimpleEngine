247bba9cbda761edd47446ca4446ef426c9c9824
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2020-03-08 09:45:46 +0400

    add carthage

---
+++ Carthage/Build/iOS/SimpleEngine.framework/Headers/SimpleEngine.h
@@ -0,0 +1,19 @@
+//
+//  SimpleEngine.h
+//  SimpleEngine
+//
+//  Created by abedalkareem omreyh on 08/03/2020.
+//  Copyright © 2020 abedalkareem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleEngine.
+FOUNDATION_EXPORT double SimpleEngineVersionNumber;
+
+//! Project version string for SimpleEngine.
+FOUNDATION_EXPORT const unsigned char SimpleEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleEngine/PublicHeader.h>
+
+
